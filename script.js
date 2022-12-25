72a66434b26a62e1c74db3aa83174b89ac1a6a33
Ubaka CHukwubuikem <ubakachukwubuikem@gmail.com>
2022-12-25 08:06:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function toggleMenu() {
-	document.querySelector('nav ul').classList.toggle('show');
-}
-document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
+const toggleButton = document.querySelector(".navbar-toggler");
+const icon = document.querySelector(".navbar-toggler-icon i");
+
+let isExpanded = false;
+
+toggleButton.addEventListener("click", () => {
+  isExpanded = !isExpanded;
+  if (isExpanded) {
+    icon.classList.remove("fa-list-ul");
+    icon.classList.add("fa-times");
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-list-ul");
+  }
+});
+
 
 
 
